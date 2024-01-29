c1c94c505445e2f0ca4249cf1e9bb026302822b8
MudasirHussain72 <mudasir7t1@gmail.com>
2024-01-29 23:21:37 +0500

    adding google SignIn for web

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginController with ChangeNotifier {
@@ -9,7 +8,7 @@ class LoginController with ChangeNotifier {
     notifyListeners();
   }
 
-  // google sig  in
+  // google sig  in for mobile
   Future<void> loginWithGoogle(BuildContext context) async {
     setLoading(true);
     // try {
@@ -65,4 +64,78 @@ class LoginController with ChangeNotifier {
     //   return;
     // }
   }
+
+  //for checking user exists or not?
+  // static Future<bool> userExists(String uid) async {
+  //   return (await FirebaseFirestore.instance.collection('users').doc(uid).get())
+  //       .exists;
+  // }
+
+  // // signInWithGoogle for web
+  // Future<User?> signInWithGoogle(BuildContext context) async {
+  //   // Initialize Firebase
+  //   await Firebase.initializeApp();
+  //   User? user;
+  //   FirebaseAuth auth = FirebaseAuth.instance;
+  //   // The `GoogleAuthProvider` can only be
+  //   // used while running on the web
+  //   GoogleAuthProvider authProvider = GoogleAuthProvider();
+
+  //   try {
+  //     final UserCredential userCredential =
+  //         await auth.signInWithPopup(authProvider);
+  //     user = userCredential.user;
+  //   } catch (e) {
+  //     print(e);
+  //   }
+
+  //   if (user != null) {
+  //     bool userAlreadyExists = await userExists(user.uid);
+  //     if (!userAlreadyExists) {
+  //       UserModel userModel = UserModel(
+  //         name: user.displayName,
+  //         email: user.email,
+  //         uid: user.uid,
+  //         role: 'player',
+  //         photo: user.photoURL,
+  //       );
+
+  //       // saving user data in database
+  //       await SignUpRepository()
+  //           .createUser(user.uid, userModel)
+  //           .then((value) async {
+  //         CollectionReference ref =
+  //             FirebaseFirestore.instance.collection('users');
+  //         await ref.doc(user!.uid).get().then((value) async {
+  //           setLoading(false);
+  //           await SessionController.saveUserInPreference(
+  //               value.data(), value['role']);
+  //           await SessionController.getUserFromPreference()
+  //               .then((value) => Navigator.pushAndRemoveUntil(
+  //                   context,
+  //                   MaterialPageRoute(
+  //                     builder: (context) => HomeScreen(),
+  //                   ),
+  //                   (route) => false));
+  //         });
+  //       });
+  //     } else {
+  //       CollectionReference ref =
+  //           FirebaseFirestore.instance.collection('users');
+  //       await ref.doc(user.uid).get().then((value) async {
+  //         setLoading(false);
+  //         await SessionController.saveUserInPreference(
+  //             value.data(), value['role']);
+  //         await SessionController.getUserFromPreference()
+  //             .then((value) => Navigator.pushAndRemoveUntil(
+  //                 context,
+  //                 MaterialPageRoute(
+  //                   builder: (context) => HomeScreen(),
+  //                 ),
+  //                 (route) => false));
+  //       });
+  //     }
+  //   }
+  //   return user;
+  // }
 }
