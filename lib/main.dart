4048c585dc39d8b1507d39b1974f30e080547da4
MudasirHussain72 <mudasir7t1@gmail.com>
2024-01-31 22:04:04 +0500

    Check-Internet-Connection-Globally

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:widget_of_the_week/controllers/services/dependency_injection.dart';
 import 'package:widget_of_the_week/shorebird_cicd/shorebird_cicd.dart';
 import 'package:widget_of_the_week/widgets/cached_video_player.dart';
 import 'package:widget_of_the_week/widgets/long_press_dialog.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
+  DependencyInjection.init();
 }
 
 class MyApp extends StatelessWidget {
@@ -12,7 +16,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       // home: CachedVideoPlayerWidget(),
       // home: ShorebirdCicdExample(),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
 import google_sign_in_ios
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
 }
